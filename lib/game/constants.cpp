9a2917d6dc5fbb0992a7a33471f668cca1dd754e
phillipf <phillip_fong@sfu.ca>
2020-02-08 00:29:43 -0800

    Added some comments

--- lib/game/constants.cpp
+++ lib/game/constants.cpp
@@ -7,12 +7,14 @@
 using constants::Constants;
 using nodes::Nodes;
 
+//Converts Constants from Json to C++ vector
 void ingestConstants(json &data, std::vector<nodes::Nodes> &listOfConstants){
     Nodes node;
     std::map<std::string, std::string> mymap;
     node.constantName=data.begin().key();
     std::vector<std::map<std::string, std::string>> nodeList;
     for (int i=0; i!=data.begin().value().size(); i++) {
+        //traverse through all key-value pairs
        for (json::iterator it = data.begin().value()[i].begin(); it != data.begin().value()[i].end(); ++it) {
             mymap[it.key()]=it.value();
             nodeList.push_back(mymap);
