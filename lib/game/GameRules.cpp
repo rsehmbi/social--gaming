ae84a0c2be1ac143f03ea3df4556032373cccd03
hba23 <hba23@sfu.ca>
2020-02-07 21:06:31 -0800

    Move Rule and GameRules to game directory and put them in the game namespace

---
+++ lib/game/GameRules.cpp
@@ -0,0 +1,22 @@
+#include "include/GameRules.h"
+
+using game::Rule;
+using game::GameRules;
+
+GameRules::GameRules() {
+}
+
+void GameRules::addRule(Rule& rule) {
+    rules.push_back(rule);
+}
+
+// Gets game rules based on JSON file
+// Calls interpreter to do so
+void GameRules::getRulesByConverting(nlohmann::json jsonRules) {
+
+}
+
+// Used for testing only
+const std::vector<Rule>& GameRules::getRules() {
+    return rules;
+}
