f6eca687b75864c31ef0800e800af86ce238e902
rsehmbi <rsehmbi@sfu.ca>
2020-03-06 14:13:48 -0800

    Factoring of interpreter.h and interpreter.cpp

--- lib/interpreter/include/interpreter.h
+++ lib/interpreter/include/interpreter.h
@@ -4,11 +4,14 @@
 #include <string>
 #include "../../jsonReader/include/dsl.h"
 #include "json.hpp"
+#include "Game.h"
 
 using game::GameState;
 using game::Constants;
 using game::Configurations;
 using game::Rule;
+using game::Variables;
+using game::VariableVariant;
 
 using nlohmann::json;
 //responsible for checking rule inside json 
@@ -29,9 +32,11 @@ namespace interpreter{
         Interpreter(json rules, json data);
         ~Interpreter();
 
+        void executeReverse(GameState &state, const Constants &constants, Configurations configurations, ListName listName);
         void executeShuffle(GameState &state, const Constants &constants, Configurations configurations, ListName listName);
         void executeSort(GameState &state, const Constants &constants, Configurations configurations, ListName listName);
-        void executeDeal(GameState &state, const Constants &constants, Configurations configurations, ListName listName);
+        void executeDeal(GameState &state, const Constants &constants,
+                Configurations configurations, Count count, From from, To to);
 
         json getUpdate();
         void processRules(json gameRules, json gameData);
--- lib/interpreter/interpreter.cpp
+++ lib/interpreter/interpreter.cpp
@@ -18,15 +18,71 @@ void Interpreter::processRules(json gameRules, json gameData){
     //calls processor to process rule block
 }
 
-void Interpreter::executeShuffle(GameState &state, const Constants &constants, Configurations &configurations, ListName listName) {
+void Interpreter::executeReverse(GameState &state, const Constants &constants, Configurations configurations, ListName listName)
+{
+    auto variable = state.variables.getVariable(listName);
+    //This is a prototype of the method not actual implementation.
+    switch ((typeid(variable)) {
+        case typeid(ListVariant):
+            std::reverese(variable.begin(), variable.end());
+            //Same for List and String
+
+            break;
+        case typeid(std::string):
+            std::reverse (variable.begin(), variable.end());
+            break;
+        case typeid(int):
+            // handle Case for more different types
+            break;
+        default:
+            // handle that case
+    }
 
 }
 
-void Interpreter::executeDeal(GameState &state, const Constants &constants, Configurations &configurations, ListName listName) {
+void Interpreter::executeShuffle(GameState &state, const Constants &constants, Configurations &configurations, ListName listName) {
+    auto variable = state.variables.getVariable(listName);
+    // Variables can be of different types
+    // Different types needs to shuffled differently
+    switch ((typeid(variable)) {
+        case typeid(ListVariant):
+            std::random_shuffle (variable.begin(), variable.end());
+            //Same for List and String
+            break;
+        case typeid(std::string):
+            std::random_shuffle (variable.begin(), variable.end());
+            break;
+        case typeid(int):
+            // handle Case for more different types
+            break;
+        default:
+            // handle that case
+    }
+}
 
+void Interpreter::executeDeal(GameState &state, const Constants &constants, Configurations &configurations,
+        ListName listName) {
+        // TO DO:
+        // Need more information
 }
 
 void Interpreter::executeSort(GameState &state, const Constants &constants, Configurations &configurations, ListName listName) {
+    auto variable = state.variables.getVariable(listName);
+
+    // Variables can be of different types
+    // Different types needs to sorted differently
+    switch ((typeid(variable)) {
+        case typeid(ListVariant):
+            std::sort( myvector.begin(), myvector.end());
+            break;
+        case typeid(std::string):
+            std::sort( myvector.begin(), myvector.end());
+            break;
+        case typeid(int):
+            // handle that case
+            break;
+        default:
+            // handle that case
 
 }
 
