b4e0fc91bb21c5999d327926fb7ca464f29574d2
qttran <qttran@sfu.ca>
2020-03-11 19:02:49 -0700

    fix GameState

--- lib/game/GameState.cpp
+++ lib/game/GameState.cpp
@@ -1,11 +1,11 @@
-#include "GameState.h"
+// #include "GameState.h"
 
-using game::GameState;
+// using game::GameState;
 
-GameState::GameState(){
+// GameState::GameState(){
 
-    // TODO : implement constructor.
-}
+//     // TODO : implement constructor.
+// }
 
-GameState::GameState(const basic_json& var, const basic_json& pPlayer, const basic_json& pAudience):
-    variables{var}, perPlayer{pPlayer}, perAudience{pAudience}{}
+// GameState::GameState(const basic_json& var, const basic_json& pPlayer, const basic_json& pAudience):
+//     variables{var}, perPlayer{pPlayer}, perAudience{pAudience}{}
--- lib/game/include/GameState.h
+++ lib/game/include/GameState.h
@@ -3,32 +3,39 @@
 
 #include <vector>
 #include "Variables.h"
-#include "PerPlayer.h"
-#include "PerAudience.h"
 #include "Messages.h"
 #include "json.hpp"
 
+using game::Variables;
+
 namespace game{
 struct Update{
-    //types of data to return
+    //content of new update to client
 }
 
-class GameState{
-    public:
-        // TODO : Remove default constructor once createGameState is done.
-        GameState();
-        GameState(const basic_json& var, const basic_json& pPlayer, const basic_json& pAudience);
-        game::Variables getUpdate(); //prototype function to return only updated data through json
-    private:
-        //TODO: need function to check for updated data
-        game::Variables variables;
-        game::PerPlayer perPlayer;
-        game::PerAudience perAudience;
-        game::Messages messages;
+struct GameState{
+    Variables variable;
+    Variables perPlayer;
+    Variables perAudience;
+    Update newUpdate;
+}
+
+// class GameState{
+//     public:
+//         // TODO : Remove default constructor once createGameState is done.
+//         GameState();
+//         GameState(const basic_json& var, const basic_json& pPlayer, const basic_json& pAudience);
+//         game::Variables getUpdate(); //prototype function to return only updated data through json
+//     private:
+//         //TODO: need function to check for updated data
+//         game::Variables variables;
+//         game::PerPlayer perPlayer;
+//         game::PerAudience perAudience;
+//         game::Messages messages;
         
 
-        // game::Variables getJSONVariables(const nlohmann::json& jsonVariables);
-};
+//         // game::Variables getJSONVariables(const nlohmann::json& jsonVariables);
+// };
 }
 
 #endif
