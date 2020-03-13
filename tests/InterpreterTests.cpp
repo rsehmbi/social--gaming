f616149069bbd16e86cd5e2fb40ec0d85c6c9852
rsehmbi <rsehmbi@sfu.ca>
2020-03-13 16:45:57 -0700

    Adds the Tests

--- tests/InterpreterTests.cpp
+++ tests/InterpreterTests.cpp
@@ -1,4 +1,38 @@
-//
-// Created by RamanSehmbi on 2020-03-13.
-//
+#include "gtest/gtest.h"
+#include "../lib/interpreter/include/interpreter.h"
+
+using interpreter::Interpreter;
+
+class InterpreterTests : public ::testing::Test {
+protected:
+    void SetUp() override {
+    }
+
+     void TearDown() override {
+
+    }
+    GameState gameState;
+};
+
+TEST_F(InterpreterTests, execute_Reverse_Test)
+{
+    // As variable class is changing will update as we update the variable class.
+}
+
+TEST_F(InterpreterTests, execute_Shuffle_Test)
+{
+
+}
+
+TEST_F(InterpreterTests, execute_Sort_Test)
+{
+
+}
+TEST_F(InterpreterTests, execute_Deal_Test)
+{
+
+}
+
+
+
 
