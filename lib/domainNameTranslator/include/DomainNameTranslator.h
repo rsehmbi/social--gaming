e0403e46f6627d1e1ffe7b864e660d7e465cfd97
Jack Chen <jyc45@sfu.ca>
2020-03-20 21:07:59 -0700

    Some scaffolding for domain function collect()

---
+++ lib/domainNameTranslator/include/DomainNameTranslator.h
@@ -0,0 +1,21 @@
+#ifndef DOMAINNAMETRANSLATOR_H
+#define DOMAINNAMETRANSLATOR_H
+
+#include "Game.h"
+#include <string>
+#include <vector>
+#include <iostream>
+
+namespace domainnametranslator {
+
+    class DomainNameTranslator {
+        public:
+            // Takes an instruction in the format of variableName.instruction 
+            // and parses this instruction.
+            // Returns a vector contatining the parsed instruction.
+            // E.g. Input = player.name Output = {player,name}
+            static std::vector<std::string> parseInstruction(std::string& instruction); 
+    };
+}
+
+#endif
