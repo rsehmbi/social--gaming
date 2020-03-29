05878a9fa7a1837fb73427a74cd98b9c8d5da169
Avneet Toor <avneett@sfu.ca>
2020-03-28 22:56:01 -0700

    initial commit to update domain translator after addition of new methods

--- lib/domainNameTranslator/include/DomainNameTranslator.h
+++ lib/domainNameTranslator/include/DomainNameTranslator.h
@@ -12,11 +12,23 @@ namespace domainnametranslator {
 
     class DomainNameTranslator {
         public:
-            // Takes an instruction in the format of variableName.instruction 
-            // and parses this instruction.
-            // Returns a vector contatining the parsed instruction.
-            // E.g. Input = player.name Output = {player,name}
+            // Takes an instruction whether in the format of variableName.instruction or
+            // variableName.instruction.function(parameters), parses this instruction
+            // and calls the appropriate functions for this instruction.
+            // Returns a Variable object.
             static Variable parseInstruction(std::string& instruction, GameState& state); 
+
+            // Takes an instruction and extracts the command chain into a vector.
+            // Returns a vector of these command chain instructions.
+            // e.g. for "players.elements.collect(player, player.weapon == weapon.beats)"
+            // returns {"players", "elements", "collect"}
+            static std::vector<std::string> parseCommandChain(std::string instruction);
+
+            // Takes an instruction and extracts the function arguments into a vector.
+            // Returns a vector of these function arguments.
+            // e.g. for "players.elements.collect(player, player.weapon == weapon.beats)"
+            // returns {"player", "player.weapon==weapon.beats"}
+            static std::vector<std::string> parseFuncArgs(std::string instruction);
     };
 }
 
